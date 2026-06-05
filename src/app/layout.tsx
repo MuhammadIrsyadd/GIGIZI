feeb4446e66732b917ed1e7cd7c8d4eee552858f
Muh Irsyad Dwi Kurniawan <muhammadirsyad258@gmail.com>
2026-06-05 09:12:51 +0700

    Update Google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   },
   manifest: "/manifest.json",
   verification: {
-    google: google-site-verification: googlea1d57ae851c0a289.html,
+    google: <meta name="google-site-verification" content="wjVywy-HvCzYXe6LiZS6HQxN1iAbipVLPyE8tYxH5e8" />,
   },
 };
 
